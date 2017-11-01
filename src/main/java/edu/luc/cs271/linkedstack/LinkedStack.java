17287794290e46e48adde0aa8604272607cfd789
Eththenet <ethannmohan@gmail.com>
2017-11-01 11:40:03 -0500

    Update LinkedStack.java

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -1,44 +1,73 @@
 package edu.luc.cs271.linkedstack;
 
-import java.util.List;
 import java.util.ArrayList;
-
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class LinkedStack<E> implements IStack<E> {
 
-  /** The topmost node of this stack. The stack gets pushed down from here. */
   private Node<E> top;
 
-  // TODO why don't we need an explicit constructor?
+  private int size = 0;
 
   @Override
   public E push(final E obj) {
-    // TODO
-    return null;
+    // DONE
+    top = new Node<>(obj, top);
+    return obj;
   }
 
   @Override
   public E peek() {
-    // TODO
-    return null;
+    // DONE
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    } else {
+      return top.data;
+    }
   }
 
   @Override
   public E pop() {
-    // TODO
-    return null;
+    // DONE
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    } else {
+      E result = top.data;
+      top = top.next;
+      return result;
+    }
   }
 
   @Override
   public boolean isEmpty() {
-    // TODO
-    return false;
+    // DONE
+    return top == null;
   }
 
   @Override
-  public List<E> asList() {
-    // TODO implement using an ArrayList preallocated with the right size
-    // TODO add any instance variable(s) required to support this
-    return null;
+public List<E> asList() {
+  final ArrayList<E> result = new ArrayList<>(size);
+  populateList(top, result); // TODO replace null with the right reference
+  return result;
+}  
+  private void populateList(final Node<E> curr, final List<E> result) {
+  if (curr == null) {
+      return;
+    }
+    result.add(curr.data);
+    populateList(curr.next, result);
   }
+}
+
+@Override
+public List<E> asFifoList() {
+  final ArrayList<E> result = new ArrayList<>(size);
+  populateFifoList(top, result); // TODO replace null with the right reference
+  return result;
+}
+
+private void populateFifoList(final Node<E> curr, final List<E> result) {
+
+}
 }
